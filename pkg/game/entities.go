e21dc749cca8af321e7d1ff6419b15d1e1b4b86c
symonk <jackofspaces@gmail.com>
2023-08-14 00:25:40 +0100

    fix build

--- pkg/game/entities.go
+++ pkg/game/entities.go
@@ -20,11 +20,12 @@ func RegisterEntities(game *Game) {
 func registerPlayer(g *Game, renderable *ecs.Component) *Player {
 	playerEntity := NewPlayer()
 	position = g.World.Manager.NewComponent()
+	renderables = g.World.Manager.NewComponent()
 	player := g.World.Manager.NewComponent()
-	g.World.Manager.NewEntity().AddComponent(player, playerEntity).AddComponent(renderable, &Renderable{Image: LoadImageFromAssets("player.png")}).AddComponent(position, &Position{X: 20, Y: 20})
+	g.World.Manager.NewEntity().AddComponent(player, playerEntity).AddComponent(renderables, &Renderable{Image: LoadImageFromAssets("player.png")}).AddComponent(position, &Position{X: 20, Y: 20})
 	players := ecs.BuildTag(player, position)
 	g.World.Tags[PlayersView] = players
-	renderables := ecs.BuildTag(renderable, position)
+	renderables := ecs.BuildTag(renderables, position)
 	g.World.Tags[RenderablesView] = renderables
 	return playerEntity
 }
@@ -32,8 +33,9 @@ func registerPlayer(g *Game, renderable *ecs.Component) *Player {
 func RegisterBosses(g *Game, renderable *ecs.Component) {
 	bossObject := &Monster{image: LoadImageFromAssets("boss1.png")}
 	bossComponent := g.World.Manager.NewComponent()
+	bossRenderable := g.World.Manager.NewComponent()
 	bossPosition := g.World.Manager.NewComponent()
-	g.World.Manager.NewEntity().AddComponent(bossComponent, bossObject).AddComponent(renderable, &Renderable{Image: LoadImageFromAssets("boss1.png")}).AddComponent(bossPosition, &Position{X: 20, Y: 5})
+	g.World.Manager.NewEntity().AddComponent(bossComponent, bossObject).AddComponent(bossRenderable, &Renderable{Image: LoadImageFromAssets("boss1.png")}).AddComponent(bossPosition, &Position{X: 20, Y: 5})
 	bosses := ecs.BuildTag(bossComponent, position)
 	g.World.Tags[BossesView] = bosses
 }
